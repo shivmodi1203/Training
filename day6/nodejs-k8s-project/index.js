79974f23ce2cae42ec7743d5e19e1e90f661df97
shivmodi1203 <shivmodi258@gmail.com>
2024-07-16 11:10:03 +0530

    Add new route

--- day6/nodejs-k8s-project/index.js
+++ day6/nodejs-k8s-project/index.js
@@ -9,3 +9,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
 });
+
+app.get('/newroute', (req, res) => {
+    res.send('This is a new route!');
+});
